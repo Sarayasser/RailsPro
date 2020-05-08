ae147e95ecf951b57bded960a6299b3a821889a5
logainhassan <logeenhassanmostafa96@gmail.com>
2020-05-08 13:43:07 -0700

    add active_admin package

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,8 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.webpacker.check_yarn_integrity = false
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
