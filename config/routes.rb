ea5f7e7ca591dc8032f4793dbdda1170f004e58f
nour <nour7anelsto7y@gmail.com>
2020-05-09 02:58:02 +0200

    routes modify

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  # get 'welcome/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :brands
   resources :categories
   resources :products
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
