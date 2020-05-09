d748c657660dca39b36c9fca96870a85b28002e4
nour <nour7anelsto7y@gmail.com>
2020-05-10 01:56:50 +0200

    edit gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -55,10 +55,6 @@ ActiveRecord::Schema.define(version: 2020_05_09_231447) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.integer "role_id"
-    t.string "image"
-
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
