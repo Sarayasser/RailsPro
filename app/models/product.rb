474ad7a1144dafa0e4f7d0abc41ce92d735fb5b9
nour <nour7anelsto7y@gmail.com>
2020-05-09 04:22:07 +0200

    handle image remove in product, brand and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ApplicationRecord
+    attr_accessor :remove_product_image
+
+    after_save :purge_product_image, if: :remove_product_image
+    private def purge_product_image
+        product_image.purge_later
+    end
+
     has_many :order_products
     has_many :orders, through: :order_products
     has_one_attached :product_image
