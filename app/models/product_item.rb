9f10b7ee4d6e34e0047178d89160b75bcec2114c
ahmed-gaber-elgamal <yakan44444@gmail.com>
2020-05-10 01:43:58 +0200

    add image to cart view

--- app/models/product_item.rb
+++ app/models/product_item.rb
@@ -1,6 +1,7 @@
 class ProductItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
+  
 
   def total_price
     product.price.to_i * quantity.to_i
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   resources :brands
   resources :categories
   
-  root 'products#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
