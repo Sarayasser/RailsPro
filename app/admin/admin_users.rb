9af9ea7d2270f58eb079cef0ab9bb6aefc558d89
logainhassan <logeenhassanmostafa96@gmail.com>
2020-05-09 16:43:53 -0700

    add remain fields to admin_user model

--- app/admin/admin_users.rb
+++ app/admin/admin_users.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register AdminUser do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :email, :password
+  permit_params :username ,:email, :password , :password_confirmation , :role
   # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
   #
   # or
@@ -15,12 +15,29 @@ ActiveAdmin.register AdminUser do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
+
+
   form do |f|
     f.inputs "Admin Details" do
+      f.input :username
       f.input :email
-      f.input :password
+      f.input :password, :hint => "Not Required if not changing the password"  , :required => false
+      f.input :password_confirmation
+      f.input :role
     end
     f.actions
+  end
+
+    # Allow form to be submitted without a password
+  controller do
+    def update
+      if params[:admin_user][:password].blank?
+        params[:admin_user].delete "password"
+        params[:admin_user].delete "password_confirmation"
+      end
+
+      super
+    end
   end
   
 end
