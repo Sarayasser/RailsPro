f1198f346bedd4a3bfd3a3551c45ddb5976d42b9
nour <nour7anelsto7y@gmail.com>
2020-05-10 02:02:35 +0200

    fix landing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_05_09_233638) do
 
   create_table "active_admin_comments", force: :cascade do |t|
