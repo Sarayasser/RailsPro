105a70adc5dc1a84429421b4379fcb1ffeb36e59
alhaithamkamal <alhaithamkamalali@gmail.com>
2020-05-11 14:44:33 +0200

    seller product model relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_10_232938) do
+ActiveRecord::Schema.define(version: 2020_05_11_102454) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2020_05_10_232938) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
+    t.integer "seller_id"
   end
 
   create_table "stores", force: :cascade do |t|
